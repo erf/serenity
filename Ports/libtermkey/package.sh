f6e9982f2d64906d5668dbba24717224a2666e01
Erlend Fagerheim <erlendf80@gmail.com>
2020-09-01 10:03:18 +0200

    add package.sh for libtermkey

---
+++ Ports/libtermkey/package.sh
@@ -0,0 +1,5 @@
+#!/bin/bash ../.port_include.sh
+port=libtermkey
+version=0.22
+useconfigure=false
+files="http://www.leonerd.org.uk/code/libtermkey/libtermkey-${version}.tar.gz libtermkey-${version}.tar.gz"
